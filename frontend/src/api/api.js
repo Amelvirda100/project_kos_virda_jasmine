7124af16752784aeacdfedee763b6fd8134f179b
Amelvirda100 <163957872+Amelvirda100@users.noreply.github.com>
2025-06-04 21:07:04 +0700

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // atau /api kalau kamu pakai prefix
+  baseURL: "https://projek-kos-backend-171192151600.us-central1.run.app", // atau /api kalau kamu pakai prefix
 });
 
 export default api;
